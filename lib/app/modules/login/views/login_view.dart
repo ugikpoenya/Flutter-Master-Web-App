f14a50ab2a8d6a646c132f4c11b71008881bd9c3
Sugeng Riyanto PC <ugikpoenya@gmail.com>
2022-03-06 15:33:55 +0700

    Update view login

--- lib/app/modules/login/views/login_view.dart
+++ lib/app/modules/login/views/login_view.dart
@@ -9,62 +9,72 @@ class LoginView extends GetView<LoginController> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: ListView(
-        padding: EdgeInsets.all(20),
-        children: [
-          SizedBox(height: 40),
-          Center(
-            child: Text(
-              "Login",
-              style: TextStyle(fontSize: 50, color: Colors.blue),
-            ),
-          ),
-          SizedBox(height: 20),
-          TextField(
-            controller: controller.emailC,
-            autocorrect: false,
-            textInputAction: TextInputAction.next,
-            keyboardType: TextInputType.emailAddress,
-            decoration: InputDecoration(
-              border: OutlineInputBorder(),
-              labelText: "Email",
-            ),
-          ),
-          SizedBox(height: 20),
-          Obx(
-            () => TextField(
-              controller: controller.passwordC,
-              autocorrect: false,
-              textInputAction: TextInputAction.next,
-              obscureText: controller.isPasswordHidden.value,
-              decoration: InputDecoration(
-                border: OutlineInputBorder(),
-                labelText: "Password",
-                suffix: IconButton(
-                  onPressed: () => controller.isPasswordHidden.toggle(),
-                  icon: Icon(
-                    Icons.remove_red_eye,
+      body: Center(
+        child: Container(
+          width: 300,
+          child: Column(
+            children: [
+              SizedBox(height: 40),
+              Center(
+                child: Text(
+                  "Login",
+                  style: TextStyle(fontSize: 50, color: Colors.blue),
+                ),
+              ),
+              SizedBox(height: 20),
+              TextField(
+                controller: controller.emailC,
+                autocorrect: false,
+                textInputAction: TextInputAction.next,
+                keyboardType: TextInputType.emailAddress,
+                decoration: InputDecoration(
+                  border: OutlineInputBorder(),
+                  labelText: "Email",
+                ),
+              ),
+              SizedBox(height: 20),
+              Obx(
+                () => TextField(
+                  controller: controller.passwordC,
+                  autocorrect: false,
+                  textInputAction: TextInputAction.next,
+                  obscureText: controller.isPasswordHidden.value,
+                  decoration: InputDecoration(
+                    border: OutlineInputBorder(),
+                    labelText: "Password",
+                    suffix: IconButton(
+                      onPressed: () => controller.isPasswordHidden.toggle(),
+                      icon: Icon(
+                        Icons.remove_red_eye,
+                      ),
+                    ),
+                    contentPadding: EdgeInsets.symmetric(
+                      vertical: 0,
+                      horizontal: 10,
+                    ),
                   ),
                 ),
               ),
-            ),
+              SizedBox(height: 20),
+              Obx(() => ElevatedButton(
+                    onPressed: () {
+                      if (controller.isLoginLoading.isFalse) {
+                        controller.login();
+                      }
+                    },
+                    child: Text(
+                      controller.isLoginLoading.isFalse
+                          ? "Login"
+                          : "Loading...",
+                    ),
+                  )),
+              SizedBox(height: 20),
+              Text("Akun Demo : "),
+              Text("email : admin@admin.com"),
+              Text("pass : admin"),
+            ],
           ),
-          SizedBox(height: 20),
-          Obx(() => ElevatedButton(
-                onPressed: () {
-                  if (controller.isLoginLoading.isFalse) {
-                    controller.login();
-                  }
-                },
-                child: Text(
-                  controller.isLoginLoading.isFalse ? "Login" : "Loading...",
-                ),
-              )),
-          SizedBox(height: 20),
-          Text("Demo : "),
-          Text("email : admin@admin.com"),
-          Text("pass : admin"),
-        ],
+        ),
       ),
     );
   }
